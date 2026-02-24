3ca0f7d2a17ee436b408801e688a0c821da5ec70
RayvenBudePXL <rayven.bude@student.pxl.be>
2026-02-24 23:27:59 +0100

    tests

--- src/test/java/be/pxl/cargo/api/CargoControllerTest.java
+++ src/test/java/be/pxl/cargo/api/CargoControllerTest.java
@@ -1,9 +1,136 @@
 package be.pxl.cargo.api;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.api.response.CargoStatistics;
+import be.pxl.cargo.domain.CargoStatus;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.service.CargoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(CargoController.class)
 public class CargoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CargoService cargoService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void addCargo_ShouldReturnCreated() throws Exception{
+        CreateCargoRequest request = new CreateCargoRequest("CARGO_1",200.0,Location.AIRPORT_X,Location.CITY_B);
+        mockMvc.perform(post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void addCargo_weightTooLow_returns400() throws Exception{
+        mockMvc.perform(post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "code": "CARGO_1",
+                        "weight": 50,
+                        "origin": "AIRPORT_X",
+                        "destination": "CITY_B"
+                    }
+                """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void addCargo_nullDestination_returns400() throws Exception{
+        mockMvc.perform(post("/cargos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                        "code": "CARGO_1",
+                        "weight": 350,
+                        "origin": "AIRPORT_X",
+                        "destination": null
+                    }
+                """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void addCargo_codeBlank_returns400() throws Exception{
+        mockMvc.perform(post("/cargos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                        "code": "",
+                        "weight": 350,
+                        "origin": "AIRPORT_X",
+                        "destination": "CITY_B"
+                    }
+                """))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void addCargo_nullOrigin_return400() throws Exception{
+
+        CreateCargoRequest request=
+                new CreateCargoRequest(
+                        "CARGO_1",
+                        200.0,
+                        null,
+                        Location.CITY_B
+                );
+        mockMvc.perform(post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+//        mockMvc.perform(post("/cargos")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content("""
+//                    {
+//                        "code": "CARGO_1",
+//                        "weight": 50,
+//                        "destination": "CITY_B"
+//                    }
+//                """))
+//                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void  getStatistics_returns200AndBody() throws Exception{
+        CargoStatistics stats = new CargoStatistics();
+        stats.setStatusCount(Map.of(CargoStatus.CREATED,2L));
+        stats.setHeaviestCargo("HEAVY");
+        stats.setAverageCargoWeight(350.0);
+        stats.setCountCargosAtWarehouseA(1);
+        stats.setTotalWeightDeliveredAtCityB(500.0);
+        when(cargoService.getCargoStatistics()).thenReturn(stats);
+
+        mockMvc.perform(get("/cargos/statistics"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.heaviestCargo").value("HEAVY"))
+                .andExpect(jsonPath("$.averageCargoWeight").value(350.0))
+                .andExpect(jsonPath("$.countCargosAtWarehouseA").value(1))
+                .andExpect(jsonPath("$.totalWeightDeliveredAtCityB").value(500.0))
+                .andExpect(jsonPath("$.statusCount.CREATED").value(2));
+    }
+
 
 
 }
--- src/test/java/be/pxl/cargo/service/CargoServiceTest.java
+++ src/test/java/be/pxl/cargo/service/CargoServiceTest.java
@@ -1,9 +1,72 @@
 package be.pxl.cargo.service;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.api.response.CargoStatistics;
+import be.pxl.cargo.domain.Cargo;
+import be.pxl.cargo.domain.CargoStatus;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.exceptions.NonUniqueCodeException;
+import be.pxl.cargo.repository.CargoRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CargoServiceTest {
+
+    @Mock
+    private CargoRepository cargoRepository;
+
+    @InjectMocks
+    private CargoService cargoService;
+
+    @Test
+    void createCargo_ShouldSaveCargo_whenCodeUnique() {
+        CreateCargoRequest request = new CreateCargoRequest("Electronics", 200.0, Location.WAREHOUSE_A, Location.CITY_B);
+        when(cargoRepository.findCargoByCode("Electronics")).thenReturn(Optional.empty());
+        cargoService.createCargo(request);
+
+        verify(cargoRepository, times(1)).save(any(Cargo.class));
+    }
+
+    @Test
+    void createCargo_shouldThrowException_whenCodeNotUnique(){
+        CreateCargoRequest request = new CreateCargoRequest("Electronics", 200.0, Location.WAREHOUSE_A, Location.CITY_B);
+        when(cargoRepository.findCargoByCode("Electronics"))
+                .thenReturn(Optional.of(mock(Cargo.class)));
+        assertThrows(NonUniqueCodeException.class,
+                () -> cargoService.createCargo(request));
+    }
+
+    @Test
+    void getCargoStatistics_shouldReturnCorrectStatistics() {
+
+        Cargo cargo1 = new Cargo("C1", 100.0,
+                Location.WAREHOUSE_A, Location.CITY_B);
+        cargo1.arrive(Location.CITY_B);
+
+        Cargo cargo2 = new Cargo("C2", 300.0,
+                Location.WAREHOUSE_A, Location.CITY_B);
+
+        when(cargoRepository.findAll())
+                .thenReturn(List.of(cargo1, cargo2));
+
+        CargoStatistics stats = cargoService.getCargoStatistics();
+
+        assertEquals("C2", stats.getHeaviestCargo());
+        assertEquals(200.0, stats.getAverageCargoWeight());
+        assertEquals(1L, stats.getCountCargosAtWarehouseA());
+        assertEquals(100.0, stats.getTotalWeightDeliveredAtCityB());
+        assertEquals(1L, stats.getStatusCount().get(CargoStatus.CREATED));
+        assertEquals(1L, stats.getStatusCount().get(CargoStatus.DELIVERED));
+    }
 
 }
